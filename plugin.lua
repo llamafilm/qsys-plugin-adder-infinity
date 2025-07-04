da6b0153b4e0f64efade4ca6e6b768519d3b9b23
Elliott Balsley <3991046+llamafilm@users.noreply.github.com>
2025-07-04 02:23:05 -0700

    remove unused files

--- plugin.lua
+++ plugin.lua
@@ -1,6 +1,3 @@
--- Adder Infinity KVM plugin
--- by Elliott Balsley
-
 -- Information block for the plugin
 --[[ #include "info.lua" ]]
 
@@ -22,31 +19,10 @@ function GetProperties()
   return props
 end
 
--- Optional function to define pins on the plugin that are not connected to a Control
-function GetPins(props)
-  local pins = {}
-  --[[ #include "pins.lua" ]]
-  return pins
-end
-
 -- Optional function to update available properties when properties are altered by the user
 function RectifyProperties(props)
   --[[ #include "rectify_properties.lua" ]]
   return props
-end
-
--- Optional function to define components used within the plugin
-function GetComponents(props)
-  local components = {}
-  --[[ #include "components.lua" ]]
-  return components
-end
-
--- Optional function to define wiring of components used within the plugin
-function GetWiring(props)
-  local wiring = {}
-  --[[ #include "wiring.lua" ]]
-  return wiring
 end
 
 -- Defines the Controls used within the plugin
